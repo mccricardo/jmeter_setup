083cf6aa74293875ca48c6d8cb031abca6a46a0c
Ricardo Castro <mcc.ricardo@gmail.com>
2015-05-08 11:53:23 +0100

    Placeholders for JMeter fetching and extraction.

--- setup_jmeter.py
+++ setup_jmeter.py
@@ -4,6 +4,32 @@ Setup JMeter locally.
 '''
 
 
+def get_jmeter():
+    '''
+    Get JMeter from repository.
+    '''
+    pass
+
+
+def extract_jmeter():
+    '''
+    Extract JMeter zip.
+    '''
+    pass
+
+
+def install_jmeter():
+    '''
+    Install JMeter locally.
+
+    Returns:
+        bool: True was setup successfully, False otherwise.
+
+    '''
+    get_jmeter()
+    extract_jmeter()
+
+
 def setup_jmeter():
     '''
     Main function to coordinate installation of JMeter and it's plugins.
@@ -12,7 +38,7 @@ def setup_jmeter():
         bool: True was setup successfully, False otherwise.
 
     '''
-    pass
+    install_jmeter()
 
 if __name__ == '__main__':
     setup_jmeter()
