bf3de6f4731ee09dc8d97f3c4b9982e8e554ac37
Ricardo Castro <mcc.ricardo@gmail.com>
2015-05-08 11:27:32 +0100

    Placeholder for setup script

---
+++ setup_jmeter.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+'''
+Setup JMeter locally.
+'''
+
+
+def setup_jmeter():
+    '''
+    Main function to coordinate installation of JMeter and it's plugins.
+
+    Returns:
+        bool: True was setup successfully, False otherwise.
+
+    '''
+    pass
+
+if __name__ == '__main__':
+    setup_jmeter()
