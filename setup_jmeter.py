7c2d7aa927d83b0529a70f4c5ed201ed43f52be0
Ricardo Castro <mcc.ricardo@gmail.com>
2015-05-08 13:09:23 +0100

    Implementation for get_jmeter

--- setup_jmeter.py
+++ setup_jmeter.py
@@ -2,13 +2,46 @@
 '''
 Setup JMeter locally.
 '''
+import config
+import requests
+from os.path import exists, join
 
 
 def get_jmeter():
     '''
     Get JMeter from repository.
     '''
-    pass
+    # URL setup
+    filename = 'apache-jmeter-' + config.VERSION + '.zip'
+    url = config.SOURCE + filename
+
+    # Get file
+    print 'Downloading JMeter. Please wait...'
+    response = requests.get(url)
+    print 'Download complete.'
+
+    # Check if request was successful
+    if not response.ok:
+        return False
+
+    # Save file on disk
+    print 'Saving JMeter. Please wait...'
+    local_file_path = join(config.PATH_TO_INSTALL, filename)
+
+    # Check if already exists. If not, write it to disk.
+    if not exists(local_file_path):
+        try:
+            with open(local_file_path, 'w') as local_file:
+                for block in response.iter_content(1024):
+                    local_file.write(block)
+            print 'Save complete.'
+        except OSError:
+            print 'Unable to open file'
+            return False
+    else:
+        print 'Already exists.'
+
+    return True
 
 
 def extract_jmeter():
